e6bc008de86e80fd4750452b565e5a398fb24efa
Theresa Campbell <t.campbell@sasktel.net>
2018-07-17 16:10:58 +0000

    Finished exercise 1.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,31 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.new
+richmond.name = "Richmond"
+richmond.annual_revenue = 1260000
+mens_apparel = false
+womens_apparel = true
+richmond.save
+
+gastown = Store.new do |s|
+  s.name = "Gastown"
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+gastown.save
+
+puts Store.count
+
+
+
+# create_table :stores do |t|
+#     t.column :name, :string
+#     t.column :annual_revenue, :integer
+#     t.column :mens_apparel, :boolean
+#     t.column :womens_apparel, :boolean
+#     t.timestamps null: false
